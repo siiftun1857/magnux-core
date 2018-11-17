e4d7ed0de7b463a1c358a782a142919ed2e9dd18
Magnus siiftun1857 Frankline <39226927@qq.com>
2018-11-17 08:36:59 +0800

    new template MXNothing

---
+++ MXNothing.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************* 
+** Magnus MAGNUX Cpp source code 
+** (c) Copyright 2011-2018 Magnus siiftun1857 Frankline 
+** This creation is licensed under Apache License Version 2.0 
+** For more information, see LICENSE and README.MD 
+**  
+** Filename: siiftun1857/magnux-core/MXNothing.cpp 
+** Author: Magnus siiftun1857 Frankline 
+** Date: 2018-11-18 11:38:12 
+** 
+*/
+#include <MXNothing.h>
+namespace magnux
+{
+namespace nothing
+{
+}//end namespace nothing 
+}//end namespace magnux 
+
+/* eof MXNothing.cpp */
---
+++ MXNothing.h
@@ -0,0 +1,32 @@
+/******************************************************************************* 
+** Magnus MAGNUX Cpp head code 
+** (c) Copyright 2011-2018 Magnus siiftun1857 Frankline 
+** This creation is licensed under Apache License Version 2.0 
+** For more information, see LICENSE and README.MD 
+**  
+** Filename: siiftun1857/magnux-core/MXNothing.h 
+** Author: Magnus siiftun1857 Frankline 
+** Date: 2018-11-18 11:37:58 
+** 
+*/
+#ifndef _MXNOTHING_H_
+#define __MXNOTHING_H
+
+
+#if _MSC_VER > 1000 
+#pragma once
+#endif // _MSC_VER > 1000
+
+//#include "MAGNUX_CORE.H"
+
+namespace magnux
+{
+namespace nothing
+{
+
+}//end namespace nothing 
+}//end namespace magnux 
+
+#endif // !ifdef _MXNOTHING_H
+
+/* eof MXNothing.h */
